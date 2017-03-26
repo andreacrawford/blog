13c9689168a783cccf2c685309cabd48d7042a50
Andrea <andrea@crawford.cc>
2017-03-25 21:43:28 -0700

    Setting up new article to publish

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,10 +1,36 @@
 
 class ArticlesController < ApplicationController
+
+  def index
+  end
+
+  def show
+    @article = Article.find(params[:id])
+  end
+
   def new
+    #@article = Article.new
+  end
+
+  def edit
   end
 
   def create
-    render plain: params[:article].inspect
+    @article = Article.new(article_params)
+
+    @article.save
+    redirect_to @article
   end
+
+  def update
+  end
+
+  def destroy
+  end
+
+  private
+    def article_params
+      params.require(:article).permit(:title, :test)
+    end
 
 end
