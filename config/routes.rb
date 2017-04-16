8136751b132286f1f640a3a23aed9f5e5ab8b9e2
Andrea <andrea@crawford.cc>
2017-04-16 15:33:56 -0700

    Added users and devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 Rails.application.routes.draw do
+    devise_scope :user do
+    get 'login', to: 'devise/sessions#new'
+    get 'register', to: 'devise/registrations#new', as: "new_user_registration"
+    delete 'logout', to: 'devise/sessions#destroy'
+  end
+
+  devise_for :users
+  resources :users
   get 'static_pages/index'
   get 'static_pages/about'
   get 'static_pages/contact'
